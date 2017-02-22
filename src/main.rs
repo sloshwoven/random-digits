bd8280c18244c196a8c21c533cfd7ceab0d2bdcf
sloshwoven <slosh.woven@gmail.com>
2017-02-22 18:24:31 -0500

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("random-digits")
+        .version("0.1.0")
+        .arg(Arg::with_name("lines")
+            .required(true)
+            .takes_value(true)
+            .value_name("LINES")
+            .help("number of lines of output to produce"))
+        .arg(Arg::with_name("width")
+            .required(true)
+            .takes_value(true)
+            .value_name("WIDTH")
+            .help("number of random digits on each output line"))
+        .get_matches();
 }
