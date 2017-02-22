e7ff1142359847bae64f87879776eb5e0847a429
sloshwoven <slosh.woven@gmail.com>
2017-02-22 18:46:00 -0500

    Validate that args are numeric

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,19 @@ fn main() {
             .required(true)
             .takes_value(true)
             .value_name("LINES")
+            .validator(is_valid_num)
             .help("number of lines of output to produce"))
         .arg(Arg::with_name("width")
             .required(true)
             .takes_value(true)
             .value_name("WIDTH")
+            .validator(is_valid_num)
             .help("number of random digits on each output line"))
         .get_matches();
+}
+
+fn is_valid_num(s: String) -> Result<(), String> {
+    s.parse::<u32>()
+        .map(|_| ())
+        .map_err(|_| format!("{} is not a valid number", s))
 }
