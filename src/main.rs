f2c9ecfa2213d7db32835984ac965f3469e93821
sloshwoven <slosh.woven@gmail.com>
2019-05-03 22:08:34 -0400

    Format

--- src/main.rs
+++ src/main.rs
@@ -19,30 +19,34 @@ fn parse_args<'a>() -> ArgMatches<'a> {
     App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(Arg::with_name("lines")
-            .required(true)
-            .takes_value(true)
-            .value_name("LINES")
-            .validator(is_valid_num)
-            .help("number of lines of output to produce"))
-        .arg(Arg::with_name("width")
-            .required(true)
-            .takes_value(true)
-            .value_name("WIDTH")
-            .validator(is_valid_num)
-            .help("number of random digits on each output line"))
+        .arg(
+            Arg::with_name("lines")
+                .required(true)
+                .takes_value(true)
+                .value_name("LINES")
+                .validator(is_valid_num)
+                .help("number of lines of output to produce"),
+        )
+        .arg(
+            Arg::with_name("width")
+                .required(true)
+                .takes_value(true)
+                .value_name("WIDTH")
+                .validator(is_valid_num)
+                .help("number of random digits on each output line"),
+        )
         .get_matches()
 }
 
 fn is_valid_num(s: String) -> Result<(), String> {
-    s.parse::<u32>()
-        .map(|_| ())
-        .map_err(|_| {
-            format!("{} is not an integer between {} and {}",
-                    s,
-                    u32::min_value(),
-                    u32::max_value())
-        })
+    s.parse::<u32>().map(|_| ()).map_err(|_| {
+        format!(
+            "{} is not an integer between {} and {}",
+            s,
+            u32::min_value(),
+            u32::max_value()
+        )
+    })
 }
 
 fn print_random_digits(lines: u32, width: u32) {
